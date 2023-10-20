607e2d8b9b8c3d72106ab92634bb469160a19585
SobolewskiWiktor <sobolsstudio@gmail.com>
2023-10-20 15:31:36 +0200

    Second part of frontend in dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -29,6 +29,14 @@
                         Settings
                     </v-btn>
                 </div>
+                <div id="dashboardMenuOption">
+                    <v-btn
+                    class="text-white h-50"
+                    variant="outlined"
+                    >
+                        Logout
+                    </v-btn>
+                </div>
             </div>
         </div>
 
@@ -47,7 +55,7 @@
             <div id="dashboardContentSavings">
                <div id="dashboardSavingsList">
                     <v-card
-                        class="mx-auto text-white"
+                        class="mx-auto text-white mt-5"
                         width="950"
                         variant="outlined"
                         >
@@ -71,6 +79,106 @@
 
                <div id="dashboardSavingsDetails">
 
+
+                    <div id="savingDeatilsTitle">
+                        <h1>SampleSaving</h1>
+                    </div>
+
+
+                    <div id="savingDetailsProgress">
+                        <h1>Progress:</h1>
+                        <v-progress-linear
+                            class="w-75 text-white"
+                            v-model="test"
+                            color="deep-purple-accent-4"
+                            height="25"
+                        >
+                            <template v-slot:default="{ value }">
+                                <strong>{{ Math.ceil(value) }}%</strong>
+                            </template>
+                        </v-progress-linear>
+                    </div>
+                    
+                    <div id="paylemtDetailsContent">
+                        <div id="savingDetailsPaymentsList">
+                            <v-card
+                            class="mx-auto text-green mt-5"
+                            width="450"
+                            variant="outlined"
+                            >
+                                <v-card-item>
+                                    <div>
+                                        <div class="text-h6 mb-1">
+                                        Incoming 500zł
+                                        </div>
+                                    </div>
+                                </v-card-item>
+                            </v-card>
+                            <v-card
+                            class="mx-auto text-red mt-5"
+                            width="450"
+                            variant="outlined"
+                            >
+                                <v-card-item>
+                                    <div>
+                                        <div class="text-h6 mb-1">
+                                        payoff 200zł
+                                        </div>
+                                    </div>
+                                </v-card-item>
+                            </v-card>
+                        </div>
+
+                        <div id="savingDetailsText">
+                            <div id="savingDetailsTextTitle">Details:</div>
+                            <div id="savingDetailsTextRow">
+                                <v-card
+                                    class="mx-auto text-gray"
+                                    width="250"
+                                    variant="tonal"
+                                    >
+                                        <v-card-item>
+                                            <div>
+                                                <div class="text-h6 mb-1">
+                                                To save: 1200zł
+                                                </div>
+                                            </div>
+                                        </v-card-item>
+                                </v-card>
+                            </div>
+                            <div id="savingDetailsTextRow">
+                                <v-card
+                                    class="mx-auto text-gray"
+                                    width="250"
+                                    variant="tonal"
+                                    >
+                                        <v-card-item>
+                                            <div>
+                                                <div class="text-h6 mb-1">
+                                                  Saved: 300zł
+                                                </div>
+                                            </div>
+                                        </v-card-item>
+                                </v-card>
+                            </div>
+                            <div id="savingDetailsTextTitleSecond">Options:</div>
+                            <div id="savingDetailsTextRow">
+                                <v-btn
+                                class="text-green"
+                                variant="tonal"
+                                >
+                                    Deposit
+                                </v-btn>
+                                <v-btn
+                                class="text-red ml-5"
+                                variant="tonal"
+                                >
+                                    Wihdraw
+                                </v-btn>
+                            </div>
+                        </div>
+                    </div>
+
                </div>
             </div>
         </div>
@@ -84,4 +192,11 @@ import { ref } from 'vue'
 
 const variants = ['elevated', 'flat', 'tonal', 'outlined']
 const color = ref('indigo')
+export default {
+    data () {
+      return {
+        test:10,
+      }
+    },
+}
 </script>
