a51b65decda400b614a83cfa7b666b72a617ecf6
SobolewskiWiktor <sobolsstudio@gmail.com>
2023-10-27 20:42:51 +0200

    Change file schema and some frontend job

---
+++ Frontend/src/components/Dashboard.vue
@@ -0,0 +1,422 @@
+<template>
+    <div id="dashboardContent">
+        <div id="dashboardMenuBar">
+            <div id="dashboardMenuBarLeftSide">
+                <img id="appLogo" src="@/Images/Logo_white.png">
+            </div>
+            <div id="dashboardMenuBarRightSide">
+               <div id="dashboardMenuBarRightButtons">
+                 <div id="buttonMenuContainer">
+                    <v-btn
+                    class="text-cyan-lighten-2"
+                     variant="tonal"
+                    >
+                        Home
+                    </v-btn>
+                 </div>
+                 <div id="buttonMenuContainer">
+                    <v-btn
+                    class="text-cyan-lighten-2"
+                    variant="tonal"
+                    >
+                        Add Saving
+                    </v-btn>
+                 </div>
+                 <div id="buttonMenuContainer">
+                    <v-btn
+                    class="text-cyan-lighten-2"
+                    variant="tonal"
+                    >
+                        Settings
+                    </v-btn>
+                 </div>
+                 <div id="buttonMenuContainer">
+                    <v-btn
+                    class="text-cyan-lighten-2"
+                    variant="tonal"
+                    >
+                        Logout
+                    </v-btn>
+                 </div>
+               </div>
+            </div>
+        </div>
+        
+        <div id="dashboardContentStatistics">
+            <div id="dashboardContentStatisticsFirstSection">
+                <div id="StatisticFirstTitle">
+                    <h1>Genneral</h1>
+                </div>
+                <div id="StatisticFirstContent">
+                    <div id="StatisticFirstContentLeft">
+                        <div id="statisticFirstContentLeftBox">
+                            <div id="statisticFirstContentLeftBoxTitle"><h1>All Piccy Banks</h1></div>
+                            <div id="statisticFirstContentLeftBoxContent">
+                                <div id="leftBoxContentNumber"><h1>8</h1></div>
+                                <div id="leftBoxContentImage"><img id="piggybankIcon" src="@/Images/piggy-bank.png"></div>
+                            </div>
+                        </div>
+                        <div id="statisticFirstContentLeftBox">
+                            <div id="statisticFirstContentLeftBoxTitle"><h1>Closed Banks</h1></div>
+                            <div id="statisticFirstContentLeftBoxContent">
+                                <div id="leftBoxContentNumber"><h1>8</h1></div>
+                                <div id="leftBoxContentImageBroken"><img id="piggybankIconBroken" src="@/Images/broken.png"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="statsticFirstContentRight">
+                        <div id="statisticFirstContentRightTitle">
+                            <h1>Saved Money</h1>
+                        </div>
+                        <div id="chartBox">
+                            <div id="chart">
+                                <apexchart type="bar" height="250" width="300" margin-left="-20px" :options="chartOptions" :series="series"></apexchart>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="dashboardContentStatisticsSecondSection">
+                <div id="StatisticFirstTitle">
+                    <h1>Total Savings</h1>
+                </div>
+                <div id="statisticSecondBoxChart">
+                    <div id="chart2">
+                        <apexchart type="area" height="330" width="450" margin-left="-20px" :options="chartOptionsYear" :series="seriesYear"></apexchart>
+                    </div>
+                </div>
+            </div>
+            <div id="dashboardContentStatisticsThirdSection">
+                <div id="StatisticFirstTitle">
+                    <h1>All savings</h1>
+                </div>
+                <div id="statisticThirdAllBox">
+                    <div id="statisticThirdAllRow">
+                          <div id="statisticThirdRowNumber">
+                             <h1>1</h1>
+                          </div>
+                          <div id="statisticThirdRowProgressBar">
+                            <v-progress-linear
+                                model-value="40"
+                                buffer-value="60"
+                                height="15px"
+                                striped
+                                color="indigo-lighten-1"
+                                ></v-progress-linear>
+                          </div>
+                          <div id="statisticThirdRowBankName">
+                            <h1>Example 1</h1>
+                          </div>
+                    </div>
+
+                    <div id="statisticThirdAllRow">
+                          <div id="statisticThirdRowNumber">
+                             <h1>2</h1>
+                          </div>
+                          <div id="statisticThirdRowProgressBar">
+                            <v-progress-linear
+                                model-value="40"
+                                buffer-value="60"
+                                height="15px"
+                                striped
+                                color="indigo-lighten-1"
+                                ></v-progress-linear>
+                          </div>
+                          <div id="statisticThirdRowBankName">
+                            <h1>Example 2</h1>
+                          </div>
+                    </div>
+
+                    <div id="statisticThirdAllRow">
+                          <div id="statisticThirdRowNumber">
+                             <h1>3</h1>
+                          </div>
+                          <div id="statisticThirdRowProgressBar">
+                            <v-progress-linear
+                                model-value="40"
+                                buffer-value="60"
+                                height="15px"
+                                striped
+                                color="indigo-lighten-1"
+                                ></v-progress-linear>
+                          </div>
+                          <div id="statisticThirdRowBankName">
+                            <h1>Example 3</h1>
+                          </div>
+                    </div>
+
+                    <div id="statisticThirdAllRow">
+                          <div id="statisticThirdRowNumber">
+                             <h1>4</h1>
+                          </div>
+                          <div id="statisticThirdRowProgressBar">
+                            <v-progress-linear
+                                model-value="40"
+                                buffer-value="60"
+                                height="15px"
+                                striped
+                                color="indigo-lighten-1"
+                                ></v-progress-linear>
+                          </div>
+                          <div id="statisticThirdRowBankName">
+                            <h1>Example 4</h1>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="dashboardContentDetails">
+            <div id="dashboardContentDetailsSavingList">
+                <div id="dashboardContentDetailsSavingListTitleBox">
+                   <div id="dashboardContentDetailsSavingListTitle"><h1>{{currentDisplayOptionDetailsSaving}} savings</h1></div>
+                   <div id="dashboardContentDetailsSavingListOptions">
+                        <v-menu
+                            transition="scale-transition"
+                            >
+                            <template v-slot:activator="{ props }">
+                                <v-btn
+                                class="text-cyan-lighten-3"
+                                color="primary"
+                                v-bind="props"
+                                >
+                                {{currentDisplayOptionDetailsSaving}}
+                                </v-btn>
+                            </template>
+
+                            <v-list>
+                                <v-list-item
+                                v-for="(item, i) in items"
+                                :key="i"
+                                >
+                                <v-list-item-title @click.prevent="changeCurrentDisplaySaving(item.title)">{{ item.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                   </div>
+                </div>
+              <div id="dashboardContentDeatilsSavingListBox">
+                    <div id="listscroll">
+
+                        <div id="dashboardContentDetailsSavingListRow">
+                            <div id="dashboardContentDetailsSavingListRowImage">
+                                <img id="savingIcon" src="../Images/regular/paper-plane.svg">
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowText">
+                                <h1>Example1</h1>
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowMoney">
+                                <div id="rowMoneyIcon">
+                                    <img id="rowMoneyIconImage" src="../Images/dollar-symbol.png">
+                                </div>
+                                <div id="rowMoneyAmmout"> <h1>200 000</h1></div>
+                            </div>
+                        </div>
+
+                        <div id="dashboardContentDetailsSavingListRow">
+                            <div id="dashboardContentDetailsSavingListRowImage">
+                                <img id="savingIcon" src="../Images/regular/paper-plane.svg">
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowText">
+                                <h1>Example1</h1>
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowMoney">
+                                <div id="rowMoneyIcon">
+                                    <img id="rowMoneyIconImage" src="../Images/dollar-symbol.png">
+                                </div>
+                                <div id="rowMoneyAmmout"> <h1>200 000</h1></div>
+                            </div>
+                        </div>
+
+                        <div id="dashboardContentDetailsSavingListRow">
+                            <div id="dashboardContentDetailsSavingListRowImage">
+                                <img id="savingIcon" src="../Images/regular/paper-plane.svg">
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowText">
+                                <h1>Example1</h1>
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowMoney">
+                                <div id="rowMoneyIcon">
+                                    <img id="rowMoneyIconImage" src="../Images/dollar-symbol.png">
+                                </div>
+                                <div id="rowMoneyAmmout"> <h1>200 000</h1></div>
+                            </div>
+                        </div>
+
+                        <div id="dashboardContentDetailsSavingListRow">
+                            <div id="dashboardContentDetailsSavingListRowImage">
+                                <img id="savingIcon" src="../Images/regular/paper-plane.svg">
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowText">
+                                <h1>Example1</h1>
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowMoney">
+                                <div id="rowMoneyIcon">
+                                    <img id="rowMoneyIconImage" src="../Images/dollar-symbol.png">
+                                </div>
+                                <div id="rowMoneyAmmout"> <h1>200 000</h1></div>
+                            </div>
+                        </div>
+
+                        <div id="dashboardContentDetailsSavingListRow">
+                            <div id="dashboardContentDetailsSavingListRowImage">
+                                <img id="savingIcon" src="../Images/regular/paper-plane.svg">
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowText">
+                                <h1>Example1</h1>
+                            </div>
+                            <div id="dashboardContentDetailsSavingListRowMoney">
+                                <div id="rowMoneyIcon">
+                                    <img id="rowMoneyIconImage" src="../Images/dollar-symbol.png">
+                                </div>
+                                <div id="rowMoneyAmmout"> <h1>200 000</h1></div>
+                            </div>
+                        </div>
+                    </div>
+              </div>
+            </div>
+            <div id="dashboardContentDetailsSavingDetails">
+                 <div id="dashboardContentDetailsSavingDetailsFirstRow">
+                    <div id="SavingDetailsFirstRowTitle">
+                        <h1>Exaple 1</h1>
+                    </div>
+                    <div id="savingDetailsFirstRowOptions">
+                        <div id="buttonDetailContainer">
+                            <v-btn
+                            class="text-light-blue-darken-3"
+                            variant="tonal"
+                            >
+                                Deposit
+                            </v-btn>
+                        </div>
+                        <div id="buttonDetailContainer">
+                            <v-btn
+                            class="text-light-blue-darken-3"
+                            variant="tonal"
+                            >
+                                Payoff
+                            </v-btn>
+                        </div>
+                        <div id="buttonDetailContainer">
+                            <v-btn
+                            class="text-light-blue-darken-3"
+                            variant="tonal"
+                            >
+                                Settings
+                            </v-btn>
+                        </div>
+                    </div>
+                    
+                 </div>
+            </div>
+        </div>
+        <div id="futter">
+            <a href="http://www.akcjabiznes.pl">AkcjaBiznes 2023 ©</a>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import "@/CSS/Dashboard.css";
+import { file, whileStatement } from "@babel/types";
+import { IncomingMessage } from "http";
+import { ref } from 'vue'
+
+export default {
+    name: 'Vue Chart',
+    data () {
+      return {
+        test:10,
+        items: [
+            { title: 'All' },
+            { title: 'Current' },
+            { title: 'Closed' },
+        ],
+        currentDisplayOptionDetailsSaving:'All',
+
+        chartOptions: {
+            chart: {
+                id: 'vuechart-example',
+                toolbar:{
+                    show:false,
+                },
+            },
+            legend:{
+                show: false,
+                colors:'#F44336',
+            },
+            dataLabels:{
+              color:'#F44336',
+            },
+            xaxis: {
+            color:'#C16E36',
+            categories: ['Exaple1', 'Exaple2', 'Exaple3', 'Exaple4', 'Exaple5', 'Exaple6', 'Exaple7', 'Exaple8'],
+             labels:{
+                show:false,
+             }
+            },
+            yaxis: {
+                show: false,
+            }
+        },
+
+        chartOptionsYear: {
+            chart: {
+                id: 'vuechart-example',
+                toolbar:{
+                    show:false,
+                },
+            },
+            legend:{
+                show: false,
+                colors:'#F44336',
+            },
+            dataLabels:{
+              color:'#F44336',
+            },
+            xaxis: {
+                color:'#F44336',
+                categories: ['STY', 'LUT', 'MAR', 'KWI', 'MAJ', 'CZE', 'LIP', 'SIE', 'WRZ', 'PAŹ', 'LIS', 'GRU'],
+                labels:{
+                    style:{
+                        colors:'#DED1B7',
+                    }
+                }
+            },
+            yaxis: {
+                labels:{
+                    style:{
+                        colors:'#DED1B7',
+                    }
+                }
+            },
+        },
+
+
+        series: [{
+            name: 'Money',
+            color:'#C16E36',
+            data: [30, 40, 45, 50, 49, 60, 70, 81]
+        }],
+
+        seriesYear: [{
+            name: 'Money',
+            color:'#41b883',
+            data: [200, 140, 345, 50, 449, 1160, 370, 81, 900, 1800, 1982, 200]
+        }]
+      }
+    },
+    methods: {
+        updateChart() {
+            const max = 90;
+            const min = 20;
+            const newData = this.series[0].data.map(() => {
+                return Math.floor(Math.random() * (max - min + 1)) + min
+            })
+        },
+        changeCurrentDisplaySaving(option:string)
+        {
+          this.currentDisplayOptionDetailsSaving = option;
+        }
+    }
+}
+</script>
