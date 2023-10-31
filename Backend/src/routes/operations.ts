4c847059725fe2433381a85cacefa67e732ae2a3
SobolewskiWiktor <sobolsstudio@gmail.com>
2023-10-31 19:08:19 +0100

    Backend for /operations and fix schema

--- Backend/src/routes/operations.ts
+++ Backend/src/routes/operations.ts
@@ -7,6 +7,38 @@ const router: Router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
+router.get("/:userid/:bankid", async (req,res) => {
+    try
+    {
+        const getter = await prisma.operations.findMany({
+            where:
+            {
+                userID: Number(req.params.userid),
+                bankID: Number(req.params.bankid)
+            }
+        })
 
+        res.status(200).json(getter)
+    }
+    catch(err)
+    {
+        res.status(500).json(err)
+    }
+} )
+
+router.post("/create", async (req,res) => {
+     try
+     {
+        let creater = await prisma.operations.create({
+            data: req.body
+        })
+
+        res.status(200).json(creater)
+     }
+     catch(err)
+     {
+        res.status(500).json(err)
+     }
+})
 
 module.exports = router;
